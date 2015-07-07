ac555eff3c07d5d8299e5442fa1c603e5acc9593
Ali Elgiadi <Aelgiadi@users.noreply.github.com>
2015-07-07 11:59:16 -0700

    Made the necessary changes
    @jeremyfelt I wrapped the register_sidebars() in a function, and attached it to widgets_init. i was a little confused, so please let me know if it was done correctly or not.

--- functions.php
+++ functions.php
@@ -1,18 +1,20 @@
 <?php	
 
- 
-register_sidebar(array(
+
+function footer_nav_widgets() {
+
+	register_sidebar(array(
 'id'	=>	'sidebar-1',
-'name' => 'footer',
+'name' => 'footer-left',
 'before_widget' => '<div id="%1$s" class="widget %2$s">',
 'after_widget' => '</div>',
 'before_title' => '<h2>',
 'after_title' => '</h2>',
 ));
 
-if ( function_exists('register_sidebar') ) {
 register_sidebar(array(
 'id'	=>	'sidebar-2',
+'name'	=>	'footer-center',
 'before_widget' => '<li id="%1$s" class="widget %2$s">',
 'after_widget' => '</li>',
 'before_title' => '<h2 class="widgettitle">',
@@ -21,7 +23,7 @@ register_sidebar(array(
 
 register_sidebar(array(
 'id'	=>	'sidebar-3',
-'name' => 'Footer',
+'name' => 'footer-right',
 'before_widget' => '<div id="%1$s" class="widget %2$s">',
 'after_widget' => '</div>',
 'before_title' => '<h2>',
@@ -29,3 +31,6 @@ register_sidebar(array(
 ));
 
 }
+add_action( 'widgets_init', 'footer_nav_widgets' );
+?>
+
