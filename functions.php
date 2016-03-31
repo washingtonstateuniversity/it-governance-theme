f3a7795da2b13566eaf99bb0b074496a9ccb1163
Ali Elgiadi <Aelgiadi@users.noreply.github.com>
2016-03-30 17:40:17 -0700

    Add 2 functions for display of Gravity Forms submissions
    Added in 2 functions to allow for gravity form submissions to be displayed and automatically populate any page the short code is placed.
    Each function determines whether the answer to section 17 in the form is either "Yes" or "No" in each entry and then applies it to which ever short code it belongs.

--- functions.php
+++ functions.php
@@ -32,5 +32,55 @@ register_sidebar(array(
 
 }
 add_action( 'widgets_init', 'footer_nav_widgets' );
+
+/* Major Project Submissions */
+
+function get_gf_submissions_major() {
+    
+    $form_id                                = '1';
+    $search_criteria['field_filters'][]     = array('key'=> '17','operator' => 'is', 'value' => 'Yes');
+    $count                                  = 0;
+    $sub_count                              = GFAPI::count_entries($form_id);
+    $entries                                = GFAPI::get_entries( $form_id, $search_criteria ); 
+        
+        while ($count <= $sub_count) {
+            echo "<div><ul><li>". $entries[$count]['2'] . "</li>";
+            echo "<li>". $entries[$count]['3'] . "</li>";
+            echo "<li>". $entries[$count]['5'] . "</li>";
+            echo "<li>". $entries[$count]['7'] . "</li>";
+            echo "<li>". $entries[$count]['8'] . "</li></ul></div>";
+
+            $count++;
+
+        }
+     
+       
+}
+add_shortcode('majorsubmissions', 'get_gf_submissions_major');
+
+/* Minor Project Submissions*/
+
+function get_gf_submissions_minor() {
+    
+    $form_id                                = '1';
+    $search_criteria['field_filters'][]     = array('key'=> '17','operator' => 'is', 'value' => 'No');
+    $count                                  = 0;
+    $sub_count                              = GFAPI::count_entries($form_id);
+    $entries                                = GFAPI::get_entries( $form_id, $search_criteria ); 
+        
+        while ($count <= $sub_count) {
+            echo "<div><ul><li>". $entries[$count]['2'] . "</li>";
+            echo "<li>". $entries[$count]['3'] . "</li>";
+            echo "<li>". $entries[$count]['5'] . "</li>";
+            echo "<li>". $entries[$count]['7'] . "</li>";
+            echo "<li>". $entries[$count]['8'] . "</li></ul></div>";
+
+            $count++;
+
+        }
+    
+}
+add_shortcode('minorsubmissions', 'get_gf_submissions_minor');
+
 ?>
 
